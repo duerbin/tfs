dd100eb97ca6b249743da7bed71a1a50e5358db1
dueb <duerbin@gmail.com>
2018-12-12 17:45:16 +0800

    from mac

---
+++ house_lines.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Author: dueb
+# @Date:   2018-12-12 16:32:24
+# @Last Modified by:   dueb
+# @Last Modified time: 2018-12-12 17:33:27
+
+
+from __future__ import print_function
+
+import math
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd 
+from sklearn import metrics 
+import tensorflow as tf 
+from tensorflow.python.data import Dataset
